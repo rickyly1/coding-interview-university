79a9b1eff0617539019f6a3ddaba792e356473ad
rickyly1 <lyr5@uci.edu>
2026-08-11 19:30:13 -0700

    marked progress - hash table section

--- java-practice/hashtable.java
+++ java-practice/hashtable.java
@@ -26,4 +26,86 @@ class HashTable {
         this.dictionary = new Entry[capacity];
         this.size = 0;
     }
+
+    private int hash(int key, int capacity) {
+        return Math.abs(key % capacity);
+    }
+
+    public void add(int key, int value) {
+        Entry newEntry = new Entry(key, value);
+        int hash = this.hash(key, this.capacity);
+
+        while (dictionary[hash] != null && dictionary[hash].key != key && !dictionary[hash].deleted) {
+            hash = (hash + 1) % this.capacity;
+        }
+
+        if (dictionary[hash] == null || dictionary[hash].deleted) {
+            this.size++;
+        }
+        
+        dictionary[hash] = newEntry;
+
+        if ((this.size * 2) >= this.capacity) {
+            this.capacity *= 2;
+            Entry[] oldDictionary = this.dictionary;
+            this.dictionary = new Entry[this.capacity];
+            this.size = 0;
+
+            for (Entry entry : oldDictionary) {
+                if (entry != null && !entry.deleted) {
+                    this.add(entry.key, entry.value);
+                }
+            }
+        }
+    }
+
+    public boolean exists(int key) {
+        int index = this.hash(key, this.capacity);
+        while (this.dictionary[index] != null) {
+            if (!this.dictionary[index].deleted && this.dictionary[index].key == key) {
+                return true;
+            }
+            index = (index + 1) % this.capacity;
+        }
+
+        return false;
+    }
+
+    public Entry get(int key) {
+        int index = this.hash(key, this.capacity);
+        while (this.dictionary[index] != null) {
+            if (!this.dictionary[index].deleted && this.dictionary[index].key == key) {
+                return this.dictionary[index];
+            }
+            index = (index + 1) % this.capacity;
+        }
+
+        return null;
+    }
+
+    public void remove(int key) {
+        int index = this.hash(key, this.capacity);
+        while (this.dictionary[index] != null) {
+            if (!this.dictionary[index].deleted && this.dictionary[index].key == key) {
+                this.dictionary[index].deleted = true;
+                this.size--;
+
+                if ((this.size) * 4 <= this.capacity) {
+                    this.capacity /= 2;
+                    Entry[] oldDictionary = this.dictionary;
+                    this.dictionary = new Entry[this.capacity];
+                    this.size = 0;
+
+                    for (Entry entry : oldDictionary) {
+                        if (entry != null && !entry.deleted) {
+                            this.add(entry.key, entry.value);
+                        }
+                    }
+                }
+
+                return;
+            }
+            index = (index + 1) % this.capacity;
+        }
+    }
 }
